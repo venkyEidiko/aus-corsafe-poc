ad6048e77467b676c5a222f3bfb93eef732e3e48
Annavaramgit <satyanarayanaraavi@123@gmail.com>
2024-10-15 15:45:41 +0530

    without cmn res jwt nd refresh token done

--- corsafe-poc-service/src/main/java/com/aus/corsafe/controller/UserRegiserController.java
+++ corsafe-poc-service/src/main/java/com/aus/corsafe/controller/UserRegiserController.java
@@ -1,17 +1,18 @@
 package com.aus.corsafe.controller;
 
 import com.aus.corsafe.dto.Login;
+import com.aus.corsafe.dto.LoginResponseCls;
 import com.aus.corsafe.entity.UserRegister;
 import com.aus.corsafe.service.LoginService;
 import com.aus.corsafe.service.UserRegisterService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class UserRegiserController {
 
     private final UserRegisterService userRegisterService;
@@ -27,18 +28,42 @@ public class UserRegiserController {
 
     @PostMapping("/register")
     public ResponseEntity<UserRegister> register(@RequestBody UserRegister userRegister) {
-
+        log.info("regiser method is executing");
         return new ResponseEntity<>(userRegisterService.register(userRegister), HttpStatus.CREATED);
 
     }
 
+    /*
+        @PostMapping("/login")
+        public ResponseEntity<String> login(@RequestBody Login login) {
+
+            log.info("login is executing && email is:" + login.getEmail() + " password is: " + login.getPassword());
+
+            return new ResponseEntity<>(loginService.tokenGenarationMethod(login), HttpStatus.OK);
+
+        }
+    */
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody Login login) {
+    public ResponseEntity<LoginResponseCls> login(@RequestBody Login login) {
 
-//        return new ResponseEntity<>(userRegisterService.register(userRegister), HttpStatus.CREATED);
+        log.info("login is executing && email is:" + login.getEmail() + " password is: " + login.getPassword());
 
-        return new ResponseEntity<>(loginService.tokenGenarationMethod(login),HttpStatus.OK);
+        return new ResponseEntity<>(loginService.tokenGenarationMethod(login), HttpStatus.OK);
 
     }
+
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testMethod() {
+        log.info("Test method is executing");
+        return new ResponseEntity<>("test successfull", HttpStatus.OK);
+    }
+
+    @PostMapping("/refreshToken/{token}")
+    public ResponseEntity<String> refreshTokenMethod(@PathVariable String token) {
+        return new ResponseEntity<>(loginService.refreshTokenGenaration(token), HttpStatus.CREATED);
+
+    }
+
 
 }
--- corsafe-poc-service/src/main/java/com/aus/corsafe/entity/UserRegister.java
+++ corsafe-poc-service/src/main/java/com/aus/corsafe/entity/UserRegister.java
@@ -1,9 +1,6 @@
 package com.aus.corsafe.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 @Data
+
 public class UserRegister {
 
      @Id
