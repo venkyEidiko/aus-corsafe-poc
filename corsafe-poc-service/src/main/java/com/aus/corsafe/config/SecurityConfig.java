5b4a254b5aa4f39a8259fae287704623768cc659
Narendra reddy <narendrapallaki@gmail.com>
2024-10-16 10:49:27 +0530

    updated register code

--- corsafe-poc-service/src/main/java/com/aus/corsafe/config/SecurityConfig.java
+++ corsafe-poc-service/src/main/java/com/aus/corsafe/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.aus.corsafe.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -44,6 +45,11 @@ private MyUserDetailasService myUserDetailasService;
     @Bean
     public AuthenticationManager authenticationManager() {
         return new ProviderManager(authenticationProvider());
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
--- corsafe-poc-service/src/main/java/com/aus/corsafe/controller/UserRegiserController.java
+++ corsafe-poc-service/src/main/java/com/aus/corsafe/controller/UserRegiserController.java
@@ -1,23 +1,23 @@
 package com.aus.corsafe.controller;
 
 import com.aus.corsafe.dto.Login;
+import com.aus.corsafe.dto.UserRegisterDto;
 import com.aus.corsafe.entity.ResponseModel;
 import com.aus.corsafe.entity.SecurityQuestion;
 import com.aus.corsafe.entity.UserRegister;
 import com.aus.corsafe.response.CommonResponse;
 import com.aus.corsafe.service.LoginService;
 import com.aus.corsafe.service.UserRegisterService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
+@Slf4j
 public class UserRegiserController {
 
     private  UserRegisterService userRegisterService;
@@ -29,21 +29,18 @@ public class UserRegiserController {
         this.userRegisterService = userRegisterService;
     }
 
-
     @PostMapping("/register")
-    public ResponseEntity<UserRegister> register(@RequestBody UserRegister userRegister) {
+    public ResponseEntity<ResponseModel<Object>> register(@RequestBody UserRegisterDto userRegister) {
 
-        return new ResponseEntity<>(userRegisterService.register(userRegister), HttpStatus.CREATED);
+         log.info("user dto {}",userRegister);
+        UserRegisterDto register = userRegisterService.register(userRegister);
 
+       return  new CommonResponse<>().prepareSuccessResponseObject(register,HttpStatus.CREATED);
     }
-
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Login login) {
 
-//        return new ResponseEntity<>(userRegisterService.register(userRegister), HttpStatus.CREATED);
-
         return new ResponseEntity<>(loginService.tokenGenarationMethod(login),HttpStatus.OK);
-
     }
     @GetMapping("/getAllSecurityQuestion")
     public  ResponseEntity<ResponseModel<Object>> getAllSecurityQuestion(){
