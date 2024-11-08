731336e21b10948729747fff4f1969f9a3c66271
Annavaramgit <satyanarayanaraavi@123@gmail.com>
2024-11-08 19:07:23 +0530

    chages in frnt code

--- corsafe-poc-application/src/App.js
+++ corsafe-poc-application/src/App.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 639f34e529398f641dd4984732e49c37df3cc856
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import BusinessProfile from './pages/BusinessProfile';
@@ -15,21 +11,12 @@ import GetProducts from './pages/GetProducts';
 import AddToCart from './pages/AddToCart';
 import CustomerAddress from './pages/CustomerAddress';
 import Payment from './pages/Payment';
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> 37e21b9 (initial commit)
-=======
->>>>>>> 639f34e529398f641dd4984732e49c37df3cc856
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 639f34e529398f641dd4984732e49c37df3cc856
+
 
       <BrowserRouter>
         <ToastContainer />
@@ -47,25 +34,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-<<<<<<< HEAD
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 37e21b9 (initial commit)
-=======
->>>>>>> 639f34e529398f641dd4984732e49c37df3cc856
+
     </div>
   );
 }
--- corsafe-poc-application/src/index.js
+++ corsafe-poc-application/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 639f34e529398f641dd4984732e49c37df3cc856
+
 import store from './store/store.js';
 import { Provider } from 'react-redux';
 
@@ -27,20 +24,11 @@ root.render(
 );
 
 
-<<<<<<< HEAD
-=======
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
->>>>>>> 37e21b9 (initial commit)
-=======
->>>>>>> 639f34e529398f641dd4984732e49c37df3cc856
+
 reportWebVitals();
