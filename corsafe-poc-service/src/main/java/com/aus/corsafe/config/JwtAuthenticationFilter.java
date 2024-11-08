f4b66a05d626f98805889cece9566ad21d83e894
Annavaramgit <satyanarayanaraavi@123@gmail.com>
2024-11-08 18:18:33 +0530

    camunda api's are prepared

--- corsafe-poc-service/src/main/java/com/aus/corsafe/config/JwtAuthenticationFilter.java
+++ corsafe-poc-service/src/main/java/com/aus/corsafe/config/JwtAuthenticationFilter.java
@@ -29,6 +29,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        log.info("jwtauthentication filter entered");
 
         try {
             String header = request.getHeader("Authorization");
